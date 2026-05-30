393c91d6e9180792d19b310272963deeae58ddf8
Ritiktyagi151 <tyagiritik321@gmail.com>
2026-05-30 16:46:18 +0530

    solveing mail issue

--- src/utils/email.utils.js
+++ src/utils/email.utils.js
@@ -2,21 +2,36 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, html, attachments }) => {
-  await resend.emails.send({
-    from: "Journal Submission <onboarding@resend.dev>",
-    to: to || process.env.ADMIN_EMAIL,
-    subject,
-    html,
-  });
+const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const result = await resend.emails.send({
+      from: "Journal Submission <onboarding@resend.dev>",
+      to: to,
+      subject,
+      html,
+    });
+    console.log("Email sent successfully:", result);
+  } catch (error) {
+    console.error("Email sending failed:", error);
+  }
 };
 
-exports.sendEmail = sendEmail;
-
-exports.sendMailToAdmin = async ({ subject, html, attachments }) => {
+// Admin ko email
+exports.sendMailToAdmin = async ({ subject, html }) => {
   return sendEmail({
     to: process.env.ADMIN_EMAIL,
     subject,
     html,
   });
 };
+
+// User ko email
+exports.sendMailToUser = async ({ userEmail, subject, html }) => {
+  return sendEmail({
+    to: userEmail,
+    subject,
+    html,
+  });
+};
+
+exports.sendEmail = sendEmail;
