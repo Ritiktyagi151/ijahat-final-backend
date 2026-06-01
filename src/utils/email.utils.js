3ce824853c169657ac39a605bcab9c94ae8f792b
Prakash Kumar <prakashkumar.twds@gmail.com>
2026-06-01 16:40:53 +0530

    Fix MongoDB connection and email configuration mail

--- src/utils/email.utils.js
+++ src/utils/email.utils.js
@@ -2,17 +2,20 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, html }) => {
+const sendEmail = async ({ to, subject, html, attachments }) => {
   try {
     const result = await resend.emails.send({
       from: "Journal Submission <onboarding@resend.dev>",
       to: to,
       subject,
       html,
+      attachments,
     });
     console.log("Email sent successfully:", result);
+    return result;
   } catch (error) {
     console.error("Email sending failed:", error);
+    throw error;
   }
 };
 
